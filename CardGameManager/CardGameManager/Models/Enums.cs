dce07f82117e0534d9a5b05f02c49f147e99661c
Kade Stephens <KStephens@student.neumont.edu>
2017-10-26 09:10:52 -0600

    Added the files that I worked on yesterday

---
+++ CardGameManager/CardGameManager/Models/Enums.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameManager.Models
+{
+    public class Enums
+    {
+        public enum Suit
+        {
+            HEARTS,
+            DIAMONDS,
+            SPADES,
+            CLUBS
+        }
+
+        public enum Value
+        {
+            ACE,
+            TWO,
+            THREE,
+            FOUR,
+            FIVE,
+            SIX,
+            SEVEN,
+            EIGHT,
+            NINE,
+            TEN,
+            JACK,
+            QUEEN,
+            KING
+        }
+    }
+}
