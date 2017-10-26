8f35952de39739655c2f089b3f958c6c4ff88148
Christopher <ChWilliams@student.neumont.edu>
2017-10-26 09:12:24 -0600

    Added the WarRules class and methods pertaining to the rules of War. Also added a Player class to the models folder.

---
+++ CardGameManager/CardGameManager/GameProcesses/WarRules.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardGameManager.Models;
+
+namespace CardGameManager.GameProcesses
+{
+    public class WarRules
+    {
+
+        //public static bool RoundWinner(CardModel playerOneCard, CardModel playerTwoCard)
+        //{
+        //    return playerOneCard.CardValue > playerTwoCard.CardValue;
+        //}
+
+        public static bool IsLoser(Player player)
+        {
+            //if (player.hand.Count <= 3)
+            //{
+            //    return true;
+            //}
+            return false;
+        }
+
+        //public static bool IsWar(CardModel playerOneCard, CardModel playerTwoCard)
+        //{
+        //    return playerOneCard.CardValue = playerTwoCard.CardValue;
+        //}
+    }
+}
