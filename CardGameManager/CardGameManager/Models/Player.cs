976171883150747a6d4a6c696edc9d53cbb10e52
Noah Tanner <NTanner@student.neumont.edu>
2017-10-27 09:45:01 -0600

    Separated  the for loop in the CheckForBust method in BlackjackRules and created the HandValue method : create BoolToImageConverter not fully tested

--- CardGameManager/CardGameManager/Models/Player.cs
+++ CardGameManager/CardGameManager/Models/Player.cs
@@ -10,7 +10,7 @@ namespace CardGameManager.Models
     {
         public string Name { get; set; }
 
-        public int balance { get; set; }
+        public int Balance { get; set; }
 
         public List<CardModel> hand = new List<CardModel>();
 
---
+++ CardGameManager/CardGameManager/Pages/Bool check page.xaml.cs
@@ -0,0 +1,47 @@
+﻿using CardGameManager.Models;
+using CardGameManager.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CardGameManager.Pages
+{
+    /// <summary>
+    /// Interaction logic for Bool_check_page.xaml
+    /// </summary>
+    public partial class Bool_check_page : Page
+    {
+        Player p1 = new Player();
+        DeckModel deck = new DeckModel();
+
+        ImageBrush cardback = new ImageBrush(new BitmapImage(new Uri(@"..\\..\\Images\\CARD_BACK.png", UriKind.RelativeOrAbsolute)));
+
+        public Bool_check_page()
+        {
+            InitializeComponent();
+            deck.Shuffle();
+            Card1.DataContext = deck.Deck[1];
+            Binding newBinding = new Binding();
+            newBinding.Path = new PropertyPath("IsFlipped");
+            BoolToImageConverter s2i = new BoolToImageConverter();
+            s2i.CardBackBrush = cardback;
+            s2i.CardBrush = new ImageBrush(new BitmapImage(new Uri(deck.Deck[1].ImagePath, UriKind.RelativeOrAbsolute)));
+            newBinding.Converter = s2i;
+            newBinding.Mode = BindingMode.OneWay;
+            newBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            BindingOperations.SetBinding(Card1, Label.BackgroundProperty, newBinding);
+
+        }
+    }
+}
