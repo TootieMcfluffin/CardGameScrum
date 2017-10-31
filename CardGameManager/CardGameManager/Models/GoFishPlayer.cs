a244cd069534228df547de191cbc2f968d603b83
Andrew <andrewsylvester117@gmail.com>
2017-10-31 08:10:34 -0600

    Added GoFishPlayer.cs

---
+++ CardGameManager/CardGameManager/Models/GoFishPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameManager.Models
+{
+	public class GoFishPlayer : Player
+	{
+
+		public int BookCount { get; set; }
+
+		public GoFishPlayer()
+		{
+			BookCount = 0;
+		}
+	}
+}
