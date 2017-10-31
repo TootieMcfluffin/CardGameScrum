7a99cd8539a0e296084ef77b32d353e804b8a723
Noah Tanner <NTanner@student.neumont.edu>
2017-10-31 08:55:01 -0600

    Started BlackjackGame class, where the game will run.

---
+++ CardGameManager/CardGameManager/GameProcesses/BlackjackGame.cs
@@ -0,0 +1,19 @@
+﻿using CardGameManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameManager.GameProcesses
+{
+    public class BlackjackGame
+    {
+        List<Player> players;
+
+        public BlackjackGame(List<Player> players1)
+        {
+            players = players1;
+        }
+    }
+}
